2e32223741d399a12a0afff0a9843e53326e608a
Jonas Sticha <jonas.sticha@tum.de>
2015-09-14 15:58:18 +0200

    Add flowColorEncoding skeleton

---
+++ VisualProject/SuperResolution/flow_color.h
@@ -0,0 +1,9 @@
+#ifndef FLOW_COLOR_H_
+#define FLOW_COLOR_H_
+
+#include <cuda_runtime.h>
+
+__global__ void createColorCoding(float* d_v1, float* d_v2, float* d_out, int w, int h);
+__global__ void createColorCoding(float* d_in, float* d_v1, float* d_v2, float* d_out, int w, int h, int nc);
+
+#endif

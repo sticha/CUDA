083845af1d5cd84eb08d4243d6aa5ec13beb5d74
Jonas Sticha <jonas.sticha@tum.de>
2015-09-15 12:11:48 +0200

    Implement projections

---
+++ VisualProject/SuperResolution/projections.cu
@@ -0,0 +1,18 @@
+#include "projections.h"
+#include <math.h>
+
+__device__ float2 projD(float2 x) {
+	float l = x.x*x.x + x.y*x.y;
+	if (l <= 1.f) {
+		return x;
+	}
+
+	l = sqrtf(l);
+	x.x /= l;
+	x.y /= l;
+	return x;
+}
+
+__device__ float projC(float x, float gamma) {
+	return (x <= gamma ? x : gamma);
+}
--- VisualProject/SuperResolution/projections.h
+++ VisualProject/SuperResolution/projections.h
@@ -3,7 +3,15 @@
 
 #include <cuda_runtime.h>
 
+/**
+ * Normale vector x if it is longer than 1
+ */
 __device__ float2 projD(float2 x);
+
+/**
+ * Clamp x to maximum value of gamma
+ */
 __device__ float projC(float x, float gamma);
 
 #endif
+

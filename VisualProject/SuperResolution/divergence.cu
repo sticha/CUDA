cca3fa78580b0fb1e3ec6d4fab53637e5255e0f7
Jonas Sticha <jonas.sticha@tum.de>
2015-09-15 11:50:41 +0200

    Refactor gradient functions to minimize redundant code

--- VisualProject/SuperResolution/divergence.cu
+++ VisualProject/SuperResolution/divergence.cu
@@ -3,18 +3,9 @@
 __device__ void gradient(float* d_u, float* d_v1, float* d_v2, int x, int y, int c, int w, int h){
 	int ind = x + y*w + c*w*h;
 
-	if (x == w - 1) {
-		d_v1[ind] = 0.f;
-	} else {
-		d_v1[ind] = d_u[ind + 1] - d_u[ind];
-	}
-
-	if (y == h - 1) {
-		d_v2[ind] = 0.f;
-	}
-	else {
-		d_v2[ind] = d_u[ind + w] - d_u[ind];
-	}
+	float2 v = gradient(d_u, x, y, c, w, h);
+	d_v1[ind] = v.x;
+	d_v2[ind] = v.y;
 }
 
 __device__ float2 gradient(float* d_u, int x, int y, int c, int w, int h) {

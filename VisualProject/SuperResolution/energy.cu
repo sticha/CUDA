d3498b0939cf4e6969fe7a72b3cbc85c4ab0a2de
Sebastian Kuemper <sebastian.kuemper@tum.de>
2015-09-28 16:51:31 +0200

    correct some mistakes

--- VisualProject/SuperResolution/energy.cu
+++ VisualProject/SuperResolution/energy.cu
@@ -63,8 +63,8 @@ __global__ void superResolutionEnergy(float* result, float* d_u1, float* d_u2, f
 	int idx = threadIdx.x + blockDim.x * blockIdx.x;
 	int tx = threadIdx.x;
 	int n = w * h;
-	int x = x % w;
-	int y = x / w;
+	int x = idx % w;
+	int y = idx / w;
 	int x_small = x / 2;
 	int y_small = y / 2;
 	int w_small = w / 2;
@@ -77,8 +77,6 @@ __global__ void superResolutionEnergy(float* result, float* d_u1, float* d_u2, f
 		// compute energy per pixel with the formula
 		// E = a * |Au1-f1|_1 + a * |Au2 - f2|_1  + |grad(u1)|_2 + |grad(u2)|_2 
 		//	+ g * ( u1 - u2 - grad(u2)*v)
-
-		// |b + Av|_1
 		for (int c = 0; c < nc; c++) {
 			int idxc = idx + n * c;
 			int idx_small = x_small + y_small * w_small + w_small * h_small * c;
@@ -92,13 +90,12 @@ __global__ void superResolutionEnergy(float* result, float* d_u1, float* d_u2, f
 			// |grad(u1)|_2 + |grad(u2)|_2
 			float2 grad_u1 = gradient(d_u1, x, y, c, w, h);
 			float2 grad_u2 = gradient(d_u2, x, y, c, w, h);
-			energy += length(grad_u1) + length(grad_u2);
+			energy += beta * length(grad_u1) + beta * length(grad_u2);
 
 			// g * |u1 - u2 - u2*v|_1
 			float u2v = grad_u2.x * d_v1[idx] + grad_u2.y * d_v2[idx];
 			energy += gamma * abs(d_u1[idxc] - d_u2[idxc] - u2v);
 		}
-		
 	}
 
 	// fill shared memory for summation

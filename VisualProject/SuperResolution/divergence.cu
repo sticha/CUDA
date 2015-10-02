1756f832ed21ee2f347d4ef19a7faa53a21c7307
Kingongo <philipp.krueger@cablemail.de>
2015-10-02 21:28:58 +0200

    Add comments to divergence.cu

--- VisualProject/SuperResolution/divergence.cu
+++ VisualProject/SuperResolution/divergence.cu
@@ -1,117 +1,153 @@
 #include "divergence.h"
 
-__global__ void calculateGradientCD(float* d_u, float2* d_v, int w, int h, int nc) {
+
+// kernel for gradient with central differences and Neumann boundary conditions (set to 0.0)
+__global__ void calculateGradientCD(float* d_in, float2* d_out, int w, int h, int nc) {
+	// get current thread index (x, y, c)
 	int x = threadIdx.x + blockDim.x * blockIdx.x;
 	int y = threadIdx.y + blockDim.y * blockIdx.y;
 	int c = threadIdx.z;
 
-	if (x >= w || y >= h || c >= nc) return;
-	int ind = x + y*w + c*w*h;
-
-	float2 grad = gradientCD(d_u, x, y, c, w, h);
-	d_v[ind] = grad;
-}
-
-__device__ float2 gradientCD(float* d_u, int x, int y, int c, int w, int h) {
-	int ind = x + y*w + c*w*h;
-	float2 ret;
-
-	if (x == w - 1 || x == 0) {
-		ret.x = 0.f;
-	} else {
-		ret.x = (d_u[ind + 1] - d_u[ind - 1]) / 2.0f;
+	// return if coordinate (x, y) not inside image
+	if (x >= w || y >= h) {
+		return;
 	}
 
-	if (y == h - 1 || y == 0) {
-		ret.y = 0.f;
-	} else {
-		ret.y = (d_u[ind + w] - d_u[ind - w]) / 2.0f;
-	}
+	// index for image pixel access
+	int ind = x + y * w + c * w * h;
 
-	return ret;
+	// compute gradient and store in output image
+	d_out[ind] = gradientCD(d_in, x, y, c, w, h);
 }
 
-__global__ void calculateGradient(float* d_u, float2* d_v, int w, int h, int nc) {
+
+// gradient per pixel with central differences and Neumann boundary conditions (set to 0.0) -> returns result as float2
+__device__ float2 gradientCD(float* d_in, int x, int y, int c, int w, int h) {
+	// index for image pixel access
+	int ind = x + y * w + c * w * h;
+
+	// define result
+	float2 result = { 0.0f, 0.0f };
+	if (x < w - 1 && x > 0) {
+		result.x = (d_in[ind + 1] - d_in[ind - 1]) / 2.0f;
+	}
+	if (y < h - 1 && y > 0) {
+		result.y = (d_in[ind + w] - d_in[ind - w]) / 2.0f;
+	}
+	return result;
+}
+
+
+// kernel for gradient with forward differences and Neumann boundary conditions (set to 0.0)
+__global__ void calculateGradient(float* d_in, float2* d_out, int w, int h, int nc) {
+	// get current thread index (x, y, c)
 	int x = threadIdx.x + blockDim.x * blockIdx.x;
 	int y = threadIdx.y + blockDim.y * blockIdx.y;
 	int c = threadIdx.z;
 
-	if (x >= w || y >= h || c >= nc) return;
+	// return if coordinate (x, y) not inside image
+	if (x >= w || y >= h) {
+		return;
+	}
+
+	// index for image pixel access
 	int ind = x + y*w + c*w*h;
 
-	float2 grad = gradient(d_u, x, y, c, w, h);
-	d_v[ind] = grad;
+	// compute gradient and store in output image
+	d_out[ind] = gradient(d_in, x, y, c, w, h);
 }
 
-__global__ void calculateGradient(float* d_u, float* d_v1, float* d_v2, int w, int h, int nc) {
+
+// kernel for gradient with forward differences and Neumann boundary conditions (set to 0.0) -> stores x derivative in out1 and y derivative in out2
+__global__ void calculateGradient(float* d_in, float* d_out1, float* d_out2, int w, int h, int nc) {
+	// get current thread index (x, y, c)
 	int x = threadIdx.x + blockDim.x * blockIdx.x;
 	int y = threadIdx.y + blockDim.y * blockIdx.y;
 	int c = threadIdx.z;
 
-	if (x >= w || y >= h || c >= nc) return;
-
-	gradient(d_u, d_v1, d_v2, x, y, c, w, h);
-}
-
-__device__ void gradient(float* d_u, float* d_v1, float* d_v2, int x, int y, int c, int w, int h){
-	int ind = x + y*w + c*w*h;
-
-	float2 v = gradient(d_u, x, y, c, w, h);
-	d_v1[ind] = v.x;
-	d_v2[ind] = v.y;
-}
-
-__device__ float2 gradient(float* d_u, int x, int y, int c, int w, int h) {
-	int ind = x + y*w + c*w*h;
-	float2 ret;
-
-	if (x == w - 1) {
-		ret.x = 0.f;
-	} else {
-		ret.x = d_u[ind + 1] - d_u[ind];
+	// return if coordinate (x, y) not inside image
+	if (x >= w || y >= h) {
+		return;
 	}
 
-	if (y == h - 1) {
-		ret.y = 0.f;
-	} else {
-		ret.y = d_u[ind + w] - d_u[ind];
-	}
-
-	return ret;
+	// compute gradient and store result in out1, out2 implicitly
+	gradient(d_in, d_out1, d_out2, x, y, c, w, h);
 }
 
-__device__ void divergence(float* d_div, float2* d_q, int x, int y, int w, int h, int c) {
+
+// gradient per pixel with forward differences and Neumann boundary conditions (set to 0.0) -> stores x derivative in out 1 and y derivative in out2
+__device__ void gradient(float* d_in, float* d_out1, float* d_out2, int x, int y, int c, int w, int h) {
+	// index for image pixel access
+	int ind = x + y*w + c*w*h;
+
+	// compute gradient
+	float2 grad = gradient(d_in, x, y, c, w, h);
+
+	// store result in output images
+	d_out1[ind] = grad.x;
+	d_out2[ind] = grad.y;
+}
+
+
+// gradient per pixel with forward differences and Neumann boundary conditions (set to 0.0) -> returns result as float2
+__device__ float2 gradient(float* d_in, int x, int y, int c, int w, int h) {
+	// index for image pixel access
+	int ind = x + y*w + c*w*h;
+
+	// define result
+	float2 result = { 0.0f, 0.0f };
+	if (x < w - 1) {
+		result.x = d_in[ind + 1] - d_in[ind];
+	}
+	if (y < h - 1) {
+		result.y = d_in[ind + w] - d_in[ind];
+	}
+	return result;
+}
+
+
+// divergence per pixel with backward differences and Dirichlet boundary conditions -> stores result in out
+__device__ void divergence(float* d_out, float2* d_in, int x, int y, int w, int h, int c) {
+	// index for image pixel access
 	int ind = x + y*w + w*h*c;
-	d_div[ind] = divergence(d_q, x, y, w, h, c);
+
+	// store result in output image
+	d_out[ind] = divergence(d_in, x, y, w, h, c);
 }
 
-__device__ float divergence(float2* d_q, int x, int y, int w, int h, int c) {
+// divergence per pixel with backward differences and Dirichlet boundary conditions -> rturn result as float
+__device__ float divergence(float2* d_in, int x, int y, int w, int h, int c) {
+	// index for image pixel access
 	int ind = x + y*w + w*h*c;
-	float ret = 0.f;
 
+	// define result
+	float2 inVal = d_in[ind];
+	float ret = inVal.x + inVal.y;
 	if (x > 0) {
-		ret += d_q[ind].x - d_q[ind - 1].x;
-	} else {
-		ret += d_q[ind].x;
+		ret -=  d_in[ind - 1].x;
 	}
 	if (y > 0) {
-		ret += d_q[ind].y - d_q[ind - w].y;
-	} else {
-		ret += d_q[ind].y;
+		ret -=  d_in[ind - w].y;
 	}
-
 	return ret;
 }
+
 
 // Computes the difference between two images in all color channels: d_diff = d_im2 - d_im1
 __global__ void imageDiff(float* d_im1, float* d_im2, float* d_diff, int w, int h) {
+	// get current thread index (x, y, c)
 	int x = threadIdx.x + blockIdx.x * blockDim.x;
 	int y = threadIdx.y + blockIdx.y * blockDim.y;
 	int c = threadIdx.z;
 
-	if (x >= w || y >= h)
+	// return if coordinate (x, y) not inside image
+	if (x >= w || y >= h) {
 		return;
+	}
 
+	// index for image pixel access
 	int idx = x + y * w + c * w * h;
+
+	// store result in output image
 	d_diff[idx] = d_im2[idx] - d_im1[idx];
 }
--- VisualProject/SuperResolution/divergence.h
+++ VisualProject/SuperResolution/divergence.h
@@ -3,19 +3,30 @@
 
 #include <cuda_runtime.h>
 
-__device__ float2 gradientCD(float* d_u, int x, int y, int c, int w, int h);
-__device__ void gradient(float* d_u, float* d_v1, float* d_v2, int x, int y, int c, int w, int h);
-__device__ float2 gradient(float* d_u, int x, int y, int c, int w, int h);
-__device__ void divergence(float* d_div, float2* d_q, int x, int y, int w, int h, int c = 0);
+// device functions per pixel
+
+// gradient per pixel with central differences and Neumann boundary conditions (set to 0.0) -> returns result as float2
+__device__ float2 gradientCD(float* d_in, int x, int y, int c, int w, int h);
+// gradient per pixel with forward differences and Neumann boundary conditions (set to 0.0) -> stores x derivative in out 1 and y derivative in out2
+__device__ void gradient(float* d_in, float* d_out1, float* d_out2, int x, int y, int c, int w, int h);
+// gradient per pixel with forward differences and Neumann boundary conditions (set to 0.0) -> returns result as float2
+__device__ float2 gradient(float* d_in, int x, int y, int c, int w, int h);
+// divergence per pixel with backward differences and Dirichlet boundary conditions -> stores result in out
+__device__ void divergence(float* d_out, float2* d_in, int x, int y, int w, int h, int c = 0);
+// divergence per pixel with backward differences and Dirichlet boundary conditions -> rturn result as float
 __device__ float divergence(float2* d_q, int x, int y, int w, int h, int c = 0);
 
-__global__ void imageDiff(float* d_im1, float* d_im2, float* d_diff, int w, int h);
 
-/**
- * CUDA Kernel, that calculates all 
- */
-__global__ void calculateGradient(float* d_u, float* d_v1, float* d_v2, int w, int h, int nc);
-__global__ void calculateGradient(float* d_u, float2* d_v, int w, int h, int nc);
-__global__ void calculateGradientCD(float* d_u, float2* d_v, int w, int h, int nc);
+// kernel functions per image
+
+// kernel for gradient with forward differences and Neumann boundary conditions (set to 0.0) -> stores x derivative in out1 and y derivative in out2
+__global__ void calculateGradient(float* d_in, float* d_out1, float* d_out2, int w, int h, int nc);
+// kernel for gradient with forward differences and Neumann boundary conditions (set to 0.0)
+__global__ void calculateGradient(float* d_in, float2* d_out, int w, int h, int nc);
+// kernel for gradient with central differences and Neumann boundary conditions (set to 0.0)
+__global__ void calculateGradientCD(float* d_in, float2* d_out, int w, int h, int nc);
+
+// kernel for difference between two images im2 - im1 -> stores result in diff
+__global__ void imageDiff(float* d_im1, float* d_im2, float* d_diff, int w, int h);
 
 #endif
